40dac87e5a5c89e15ad855d72b96364abfeea953
lh83mail <lh83mail@126.com>
2017-03-13 18:29:17 +0800

    test

---
+++ thallo-server/src/main/java/info/halo/thallo/Application.java
@@ -0,0 +1,32 @@
+package info.halo.thallo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by lihong on 17-3-13.
+ */
+@SpringBootApplication
+@EnableAutoConfiguration
+@EnableConfigServer
+@Configuration
+@RestController
+public class Application {
+    @Value("${config.name}")
+    String name = "World";
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello " + name;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
