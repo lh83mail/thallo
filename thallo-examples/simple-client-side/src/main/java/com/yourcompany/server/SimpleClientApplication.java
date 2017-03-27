fa6a3d270dabc548c71878a1207b4d3b46e40911
lh83mail <lh83mail@126.com>
2017-03-27 19:46:51 +0800

    sample

--- thallo-examples/simple-client-side/src/main/java/com/yourcompany/server/SimpleClientApplication.java
+++ thallo-examples/simple-client-side/src/main/java/com/yourcompany/server/SimpleClientApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
@@ -16,7 +17,7 @@ import org.springframework.context.annotation.Configuration;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
-//@EnableCircuitBreaker
+@EnableCircuitBreaker
 @EnableFeignClients
 @Configuration
 @ComponentScan(basePackages = {"com.yourcompany", "com.yourcompany.service"})
