286df039fd0e626146a9f48768d74c011fbc713e
lh83mail <lh83mail@126.com>
2017-03-29 16:17:19 +0800

    修改包结构

--- thallo-server/src/main/java/org/halo/thallo/server/Application.java
+++ thallo-server/src/main/java/org/halo/thallo/server/Application.java
@@ -1,22 +1,15 @@
 package org.halo.thallo.server;
 
-import com.netflix.appinfo.InstanceInfo;
 import com.netflix.discovery.EurekaClient;
 import com.netflix.discovery.shared.Applications;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 /**
  * Created by lihong on 17-3-13.
@@ -28,7 +21,7 @@ import java.util.Set;
 @RestController
 public class Application {
     @Value("${config.name}")
-    String name = "World";
+    private String name = "World";
 
     @Autowired
     private EurekaClient discoveryClient;
