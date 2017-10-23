db36800f44625154405030cf79ffb9974eab4254
lh83mail <lh83mail@126.com>
2017-10-23 15:03:10 +0800

    deeee

---
+++ mmr/src/main/java/org/halo/thallo/mmr/core/mapper/InsertProvider.java
@@ -0,0 +1,25 @@
+package org.halo.thallo.mmr.core.mapper;
+
+import org.apache.ibatis.jdbc.SQL;
+import org.halo.thallo.mmr.core.model.Attribute;
+import org.halo.thallo.mmr.core.model.DataObject;
+
+import java.util.Map;
+
+/**
+ * Created by lihong on 17-10-19.
+ */
+public class InsertProvider {
+    public String buildSQL(DataObject dataObject, Map<String, Object> params) {
+        SQL sql = new SQL(){{
+            INSERT_INTO(dataObject.getName());
+            Iterable<Attribute> attributes = dataObject.getAttributes();
+            attributes.forEach(attr -> {
+                if (attr.isInsertable()) {
+                    VALUES(attr.getName(), "#{" + attr.getName() + "}");
+                }
+            });
+        }};
+        return  sql.toString();
+    }
+}
