faef9fa9f1cea3916af21635c1f5f490fea2f46e
lh83mail <lh83mail@126.com>
2020-04-21 08:26:29 +0800

    add new

---
+++ console/src/views/gateway/components/PathPredicates.vue
@@ -0,0 +1,30 @@
+<template>
+  <el-collapse-item title="路径匹配(Path)" name="Path">
+    <el-form-item label="patterns">
+      <el-input name="patterns" />
+    </el-form-item>
+    <el-form-item label="完全匹配">
+      <el-switch :value="true" name="matchOptionalTrailingSeparator" />
+    </el-form-item>
+  </el-collapse-item>
+</template>
+
+<script>
+export default {
+  props: {
+    item: {
+      type: Object,
+      required: true
+    }
+  },
+  // data() {
+  //   return {
+  //     item: this.item
+  //   }
+  // },
+
+  created() {
+    console.log('LLLLLL', this.item)
+  }
+}
+</script>
--- console/src/views/gateway/index.vue
+++ console/src/views/gateway/index.vue
@@ -15,9 +15,72 @@
       <el-table-column prop="createAt" label="创建时间" width="180" :formatter="dateFormater" />
       <el-table-column prop="updateAt" label="更新时间" width="180" :formatter="dateFormater" />
       <el-table-column prop="description" label="描述" />
+      <el-table-column fixed="right" label="操作" width="400">
+        <template v-slot:default="scope">
+          <el-button type="text" size="small">过滤器</el-button>
+          <el-button type="text" size="small">前置拦截</el-button>
+        </template>
+      </el-table-column>
     </el-table>
-    <pagination v-show="total>0" :total="total" :page.sync="listQuery.page" :limit.sync="listQuery.limit" @pagination="getList" />
+    <pagination
+      v-show="total>0"
+      :total="total"
+      :page.sync="listQuery.page"
+      :limit.sync="listQuery.limit"
+      @pagination="getList"
+    />
 
+    <el-dialog title="收货地址" :visible.sync="dialogFormVisible">
+      <el-form :model="form" :rules="rules" :label-width="formLabelWidth">
+        <el-form-item label="路由标识" prop="routeId">
+          <el-input v-model="form.routeId" />
+        </el-form-item>
+        <el-form-item label="URI" prop="uri">
+          <el-input v-model="form.uri" />
+        </el-form-item>
+
+        <el-row>
+          <el-col :span="12">
+            <el-form-item label="序号" prop="orders">
+              <el-input-number v-model="form.orders" :min="0" />
+            </el-form-item>
+          </el-col>
+          <el-col :span="12">
+            <el-form-item label="地址状态" prop="status">
+              <el-select v-model="form.status" placeholder="选择地址状态">
+                <el-option label="可用" value="Y" />
+                <el-option label="禁用" value="N" />
+              </el-select>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-form-item>
+          <el-tabs v-model="form.defaultTab">
+            <el-tab-pane label="路由断言(Predicates)" name="first">
+              <el-collapse>
+
+                <component :is="item.name + 'Predicates'" v-for="item in form.predicates" :key="item.name" :item="item" />
+                <!-- <el-collapse-item title="主机名匹配(Host)" name="Host">
+                      <el-form-item label="patterns" >
+                        <el-input  name="patterns"/>
+                      </el-form-item>
+                    </el-collapse-item> -->
+
+              </el-collapse>
+            </el-tab-pane>
+            <el-tab-pane label="过滤器(Filters)" name="second">配置管理</el-tab-pane>
+          </el-tabs>
+        </el-form-item>
+        <el-form-item label="描述">
+          <el-input v-model="form.description" type="textarea" :autosize="{ minRows: 1, maxRows: 3}" />
+        </el-form-item>
+
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="onSubmit">确 定</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
@@ -25,9 +88,10 @@
 import Pagination from '@/components/Pagination'
 import { searchRoutes } from '@/api/gateway'
 import { format } from 'date-fns'
+import PathPredicates from './components/PathPredicates'
 
 export default {
-  components: { Pagination },
+  components: { Pagination, PathPredicates },
   data() {
     return {
       tableKey: 'id',
@@ -42,6 +106,31 @@ export default {
         title: undefined,
         type: undefined,
         sort: '+id'
+      },
+
+      formLabelWidth: '80px',
+      dialogFormVisible: true,
+      form: {
+        id: null,
+        uri: '',
+        routeId: '',
+        predicates: [
+          { name: 'Path', args: { patterns: '/payable/**' }}
+        ],
+        filters: [],
+        description: null,
+        orders: 0,
+        status: 'Y',
+        defaultTab: 'first'
+      },
+
+      rules: {
+        routeId: [
+          { required: true, message: '请输入路由标识', trigger: 'blur' }
+        ],
+        uri: [
+          { required: true, message: '请输入uri', trigger: 'blur' }
+        ]
       }
     }
   },
@@ -85,6 +174,10 @@ export default {
     getSortClass: function(key) {
       const sort = this.listQuery.sort
       return sort === `+${key}` ? 'ascending' : 'descending'
+    },
+
+    onSubmit: function() {
+      console.log(this.form)
     }
   }
 }
