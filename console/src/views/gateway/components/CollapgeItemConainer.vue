97ee4cceb347fc9969df88c3f0e3dea2d0278cf8
lh83mail <lh83mail@126.com>
2020-05-08 01:16:28 +0800

    add new

---
+++ console/src/views/gateway/components/CollapgeItemConainer.vue
@@ -0,0 +1,25 @@
+<template>
+  <el-collapse-item name="Path">
+    <template slot="title">
+      {{ title }}
+      <slot name="title" />
+      <el-popconfirm title="确定吗？">
+        <i slot="reference" class="el-icon-remove ml-sm text-danger width-1" @click.stop="fireCloseEvent" />
+      </el-popconfirm>
+    </template>
+    <slot />
+  </el-collapse-item>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String
+  },
+  methods: {
+    fireCloseEvent() {
+      this.$emit('close')
+    }
+  }
+}
+</script>
